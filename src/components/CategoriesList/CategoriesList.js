cc739ed5ec5158b22649838bdbbf0360c8305a1d
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2019-04-12 14:56:06 +0100

    Updated Categories and Product List components to render provided data

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -1,5 +1,22 @@
-const CategoriesList = () => (
-	<div className="categories-list">Categories List</div>
-);
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './CategoriesList.styles.css';
+
+const CategoriesList = ({ categories }) => {
+	return (
+		<ul className="categoriesList">
+			{categories
+				.filter(category => category.hidden === false)
+				.map(({ id, title }) => (
+					<li key={id}>{title}</li>
+				))}
+		</ul>
+	);
+};
+
+CategoriesList.propTypes = {
+	categories: PropTypes.array.isRequired,
+};
 
 export default CategoriesList;
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,3 +1,21 @@
-const ProductsList = () => <div className="products-list">Products List</div>;
+import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+
+import './ProductsList.styles.css';
+
+const ProductsList = ({ products }) => {
+	return (
+		<ul className="productsList">
+			{_.filter(products, 'is_for_sale').map(({ id, title }) => (
+				<li key={id}>{title}</li>
+			))}
+		</ul>
+	);
+};
+
+ProductsList.propTypes = {
+	products: PropTypes.object,
+};
 
 export default ProductsList;
