8ccfa72d145f253fb641fae9d7e5cd8be236aab4
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2019-04-18 17:06:48 +0100

    Updated ProductDetail test suite

--- src/components/ProductDetail/ProductDetail.test.js
+++ src/components/ProductDetail/ProductDetail.test.js
@@ -1,7 +1,10 @@
 import ProductDetail from './ProductDetail';
 
+const title = 'title';
+const description = 'description';
+
 describe('ProductDetail component', () => {
 	test('renders without crashing', () => {
-		shallow(<ProductDetail />);
+		shallow(<ProductDetail title={title} description={description} />);
 	});
 });
